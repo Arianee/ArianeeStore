690e9047c0fe6691213fb9e35da2658f24e203e3
Maxime <maxime@vaugam.com>
2019-03-15 11:28:18 +0100

    add creditHistory can only be called by store

--- contracts/ArianeeCreditHistory.sol
+++ contracts/ArianeeCreditHistory.sol
@@ -1,20 +1,31 @@
 pragma solidity 0.5.1;
 
-contract ArianeeCreditHistory{
+import "@0xcert/ethereum-utils-contracts/src/contracts/permission/ownable.sol";
+
+contract ArianeeCreditHistory is Ownable{
     
     
-    mapping(address => mapping(uint256=>CreditBuy[])) public creditHistory;
+  mapping(address => mapping(uint256=>CreditBuy[])) public creditHistory;
   
   mapping(address => mapping(uint256=>uint256)) public historyIndex;
+  
+  address public arianeeStoreAddress;
   
   struct CreditBuy{
       uint256 price;
       uint256 quantity;
   }
   
-  //TODO add only callable by stores
+   modifier onlyStore(){
+        require(msg.sender == arianeeStoreAddress, 'not called by store');
+        _;
+    }
   
-  function addCreditHistory(address _spender, uint256 _price, uint256 _quantity, uint256 _type) public{
+  function changeArianeeStoreAdress(address _newArianeeStoreAdress) onlyOwner() public{
+      arianeeStoreAddress = _newArianeeStoreAdress;
+  }
+  
+  function addCreditHistory(address _spender, uint256 _price, uint256 _quantity, uint256 _type) public onlyStore() {
       
       CreditBuy memory _creditBuy = CreditBuy({
           price: _price,
@@ -25,7 +36,7 @@ contract ArianeeCreditHistory{
       
   }
 
-    function getCreditPrice(address _spender, uint256 _type) public returns (uint256){
+    function getCreditPrice(address _spender, uint256 _type) public onlyStore() returns (uint256){
         uint256 _index = historyIndex[_spender][_type];
         uint256 price = creditHistory[_spender][_type][_index].price;
         creditHistory[_spender][_type][_index].quantity = creditHistory[_spender][_type][_index].quantity-1;
