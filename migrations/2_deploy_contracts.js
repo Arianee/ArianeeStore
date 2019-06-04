dc45d640f36ee2c3fe6b7f6e26ca14d6eab16461
Maxime <maxime@vaugam.com>
2019-06-04 11:34:20 +0200

    update and add project configuration files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var Arianee = artifacts.require("./arianeeStore.sol");
+var Arianee = artifacts.require('./arianeeStore.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(Arianee,'0x841d01c859355fde1f36a9ce0951de5a10802110','0x841d01c859355fde1f36a9ce0951de5a10802110','0x841d01c859355fde1f36a9ce0951de5a10802110');
