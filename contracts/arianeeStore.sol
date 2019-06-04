484cb8ec7ffb9114028053430b65d6a504df66ed
Maxime <maxime@vaugam.com>
2019-06-04 10:34:01 +0200

    update solidity version to 0.5.6

--- contracts/arianeeStore.sol
+++ contracts/arianeeStore.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.1;
+pragma solidity 0.5.6;
 
 import "@0xcert/ethereum-utils-contracts/src/contracts/math/safe-math.sol";
 import "@0xcert/ethereum-erc721-contracts/src/contracts/nf-token-metadata-enumerable.sol";
